b232f3c930cf5f1ec3e4a1d4f2ab18433d5bab6b
Esmelyn Baez <47229162+Esmelyn04@users.noreply.github.com>
2025-10-21 17:55:44 -0400

    Update 4-arrays.js

--- JavaScript/4-arrays.js
+++ JavaScript/4-arrays.js
@@ -174,5 +174,6 @@ console.log(nestedArr[1][0]); // Output: 2
 
 const numberClusters = [ [1,2], [3,4], [5,6] ]
 
-const target = numberClusters[-1][-1]
+const target = numberClusters[2][1]
 console.log(target) // Output: 6
+
